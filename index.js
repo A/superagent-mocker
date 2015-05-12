88a632857de7506095eec4eec6f3b6e8e3dacf89
Shuvalov Anton <anton_shuvalov@me.com>
2015-05-12 19:25:30 +0300

    move route into the index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 /**
  * Dependencies
  */
-var pathToRegexp = require('path-to-regexp');
-var Route        = require('./route');
+var pathtoRegexp = require('path-to-regexp');
 
 /**
  * Expose public API
@@ -78,3 +77,38 @@ function defineRoute(method, url, callback) {
   }));
   return mock;
 }
+
+/**
+ * Route with given url
+ */
+var Route = function Route(state) {
+  this.url    = state.url;
+  this.fn     = state.callback;
+  this.method = state.method;
+  this.regexp = pathtoRegexp(this.url, this.keys = []);
+};
+
+/**
+ * Match route with given url
+ */
+Route.prototype.match = function(method, url, body) {
+  if (this.method !== method) return false;
+  var params = {};
+  var m = this.regexp.exec(url);
+  if (!m) return false;
+  for (var i = 1, len = m.length; i < len; ++i) {
+    var key = this.keys[i - 1];
+    var val = m[i];
+    if (val !== undefined || !(hasOwnProperty.call(params, key.name))) {
+      params[key.name] = val;
+    }
+  }
+  var route = this;
+  return function() {
+    return route.fn({
+      url: url,
+      params: params || {},
+      body: body || {}
+    });
+  };
+};
